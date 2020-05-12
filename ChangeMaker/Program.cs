8a4f90ef3d42a0a0f58a223e575b0445cf31bd45
Tyler Bates <Fondlytyler@gmail.com>
2020-05-12 16:23:15 -0700

    add front end logic and UI.

--- ChangeMaker/Program.cs
+++ ChangeMaker/Program.cs
@@ -9,10 +9,12 @@ namespace Program
     public static void Main()
     {
       ChangeBack changeMaker = new ChangeBack();
-      Console.WriteLine("Welcome to the Change-Machine!\n\nEnter an Amount in dollars and we'll break it down into change:");
+      Console.WriteLine("Welcome to the Change-Machine!\n\nEnter an Amount in change and we'll break it down to it's smallest number of coins possible:");
       string input = Console.ReadLine();
       ChangeBack.GetInput(input);
-
+      ChangeBack.ChangeIt();
+      Console.WriteLine("Your change consists of:\n" + ChangeBack.Quarters + " quarters\n" + ChangeBack.Dimes + " dimes\n" + ChangeBack.Nickels + " nickels\n" + ChangeBack.Pennies + " pennies");
+      Console.WriteLine("Keep the change ya filthy animal.");
     }
   }
 }
